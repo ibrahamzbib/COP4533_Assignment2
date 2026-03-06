120e72043bd5bb34ca1041a2659c1fd6ad3ac5f7
ibrahamzbib <ibraham.zbib@gmail.com>
2026-03-06 13:37:13 -0500

    fifo

--- src/cache_sim.py
+++ src/cache_sim.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 def main():
     if len(sys.argv) < 2:
@@ -11,9 +12,27 @@ def main():
 
     assert len(requests) == m
 
-    print(f"FIFO  : 0")
+    print(f"FIFO  : {simulate_fifo(requests,k)}")
     print(f"LRU   : 0")
     print(f"OPTFF : 0")
+
+def simulate_fifo(requests,k):
+    cache = set()
+    order = deque()
+    misses = 0
+
+    for r in requests:
+        if r in cache:
+            continue
+        misses += 1
+        if len(cache) == k:
+            evict = order.popleft()
+            cache.remove(evict)
+        cache.add(r)
+        order.append(r)
+
+    return misses
+
 
 if __name__ == "__main__":
     main()
